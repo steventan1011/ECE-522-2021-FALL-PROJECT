8fa9ddaa40beed13897dd5cc4810d47c63cd17f5
Ritaa-Zhang <1054508363@qq.com>
2021-12-04 19:29:56 -0700

    Insert is finished

--- rb_tree/src/avl_tree.rs
+++ rb_tree/src/avl_tree.rs
@@ -72,7 +72,31 @@ impl AVLTree {
         if balance_factor.abs() > 1.0 {
             println!("unbalanced: {}", balance_factor);
         }
-        println!("insert node: {}", temp_node.borrow().value.clone());
+
+        //println!("insert node: {}", temp_node.borrow().value.clone());
+
+        // maintain
+        // case LL: right rotate
+        if balance_factor > 1.0 && self.get_balance_factor(&temp_node.borrow().left.clone().unwrap()) >= 0.0 {
+            return Some(self.right_rotate(temp_node))
+        }
+
+        // case RR: left rotate
+        if balance_factor < -1.0 && self.get_balance_factor(&temp_node.borrow().right.clone().unwrap()) <= 0.0 {
+            return Some(self.left_rotate(temp_node))
+        }
+
+        // case LR: left rotate + right rotate
+        if balance_factor > 1.0 && self.get_balance_factor(&temp_node.borrow().left.clone().unwrap()) < 0.0 {
+            temp_node.borrow_mut().left = Some(self.left_rotate(temp_node.borrow_mut().left.unwrap()));
+            return Some(self.right_rotate(temp_node))
+        }
+
+        // case RL: right rotate + left rotate
+        if balance_factor < -1.0 && self.get_balance_factor(&temp_node.borrow().right.clone().unwrap()) > 0.0 {
+            temp_node.borrow_mut().right = Some(self.right_rotate(temp_node.borrow_mut().right.unwrap()));
+            return Some(self.left_rotate(temp_node))
+        }
         Some(temp_node)
     }
 
@@ -113,6 +137,54 @@ impl AVLTree {
             self._is_balanced(node.unwrap().borrow_mut().left.clone()) &&
                 self._is_balanced(node.unwrap().borrow_mut().right.clone())
         }
+    }
+
+    //                 y                                     x
+    //               /    \                                 /   \
+    //              x     T4      right rotate (y)         z     y
+    //           /   \            ---------------->       / \   / \
+    //          z     T3             return x            T1 T2 T3 T4
+    //         /   \
+    //        T1   T2
+    fn right_rotate(&self, y: AVLTreeNode) -> AVLTreeNode {
+        let mut x = y.borrow().left.clone().unwrap();
+        let mut T3 = x.borrow().right.clone().unwrap();
+
+        // right rotate
+        x.borrow_mut().right = Some(y);
+        y.borrow_mut().left = Some(T3);
+
+        // update height of x and y
+        y.borrow_mut().height = self.get_left_height(&y)
+            .max(self.get_right_height(&y)) + 1;
+        x.borrow_mut().height = self.get_left_height(&x)
+            .max(self.get_right_height(&x)) + 1;
+
+        return x
+    }
+
+    //                 y                                        x
+    //               /    \                                   /   \
+    //              T1     x        left rotate (y)          y     z
+    //                   /   \      ---------------->       / \   / \
+    //                 T2     z        return x            T1 T2 T3 T4
+    //                       /  \
+    //                      T3   T4
+    fn left_rotate(&self, y: AVLTreeNode) -> AVLTreeNode {
+        let mut x = y.borrow().right.clone().unwrap();
+        let mut T2 = x.borrow().left.clone().unwrap();
+
+        // left rotate
+        x.borrow_mut().left = Some(y);
+        y.borrow_mut().right = Some(T2);
+
+        // update height of x and y
+        y.borrow_mut().height = self.get_left_height(&y)
+            .max(self.get_right_height(&y)) + 1;
+        x.borrow_mut().height = self.get_left_height(&x)
+            .max(self.get_right_height(&x)) + 1;
+
+        return x
     }
 
 
